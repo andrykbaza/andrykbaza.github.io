4e9a5897ff6be49de9594d9bc2560e262d0ef3b7
andrykbaza <andrykvasko@gmail.com>
2024-11-02 00:44:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById("booking-form").addEventListener("submit", function(event) {
     event.preventDefault();
     const name = document.getElementById("name").value;
+    const phone = document.getElementById("phone").value;
     const date = document.getElementById("date").value;
     const time = document.getElementById("time").value;
 
-    const confirmationMessage = `Дякуємо, ${name}! Ваш запис на ${date} о ${time} прийнятий.`;
+    const confirmationMessage = `Дякуємо, ${name}! Ваш запис на ${date} о ${time} прийнятий. Ми зв'яжемося з вами за номером ${phone}.`;
     document.getElementById("confirmation-message").innerText = confirmationMessage;
 
     // Очистити форму
